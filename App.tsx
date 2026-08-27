ddc44c64b2cfc9496cc130497cf22b1d9fc430b8
vlopes <viniciusrodrigueslopes25@gmail.com>
2026-08-27 15:08:55 -0300

    feat: iniciando projeto de programação para dispositivos móveis

--- App.tsx
+++ App.tsx
@@ -1,20 +1,35 @@
+import { Ionicons } from '@expo/vector-icons';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { ScrollView, StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <ScrollView style={styles.container}>
+      <View style={styles.header}>
+        <Text>Café do Código</Text>
+        <Text>Seu café, uma linha por vez</Text>
+      </View>
+      
+      <View>
+        <Ionicons name='person' size={20} color={"#2f2d2c"}></Ionicons>
+      </View>
+    </ScrollView>
   );
 }
 
-const styles = StyleSheet.create({
+const styles = StyleSheet.create ({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: '#f9f9f9'
+
   },
-});
+  header: {
+    width: '100%',
+    paddingTop: 60,
+    paddingHorizontal: 24,
+    paddingBottom: 20,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center'
+  }
+})
